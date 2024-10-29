5ff9a2ce7bf46c11dc61f920003731e08bf4ef80
nikson0013 <sorokin.na.2006@gmail.com>
2024-10-29 12:58:44 +0300

    commit

---
+++ js/burgerFunction.js
@@ -0,0 +1,43 @@
+const burger = document.querySelectorAll('.burger'),
+    menu = document.querySelectorAll('.header-nav'),
+    tel = document.querySelectorAll('.header__tel-icon'),
+    navItem = document.querySelectorAll('.nav-link');
+
+function toggleBurger() {
+    burger.forEach(item => {
+        if(item.classList.contains('hidden')) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+        }
+    })
+    tel.forEach(item => {
+        if(item.classList.contains('hidden')) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+        }
+    })
+    menu.forEach(item => {
+        if(item.classList.contains('active')) {
+            item.classList.remove('active');
+        } else {
+            item.classList.add('active');
+        }
+    })
+    navItem.forEach(item => {
+        if(item.classList.contains('active')) {
+            item.classList.remove('active');
+        } else {
+            item.classList.add('active');
+        }
+    })
+}
+
+burger.forEach(item => {
+    item.addEventListener('click', toggleBurger);
+})
+
+navItem.forEach(item => {
+    item.addEventListener('click', toggleBurger);
+})
